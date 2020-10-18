46d1f75934be609d9b514d51a3660907b702ea94
Ryan Vuscan <ryanvcsp@gmail.com>
2020-10-18 11:59:45 -0400

    Added Theme for MUI to app with provider

--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { CssBaseline, ThemeProvider } from '@material-ui/core';
+import Theme from './Theme';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CssBaseline />
+    <ThemeProvider theme={Theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
